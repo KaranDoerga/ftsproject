48a58d7ef0e87a468458b4bc687bb5cc237e81db
KaranDoerga <s1217356@student.windesheim.nl>
2025-05-29 13:37:03 +0200

    Eigen home page aangemaakt met mijn eigen routes in de navbar (nog niet werkend)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,6 @@
-<?php
+<x-app-layout>
+    <div class="container mx-auto mt-6">
+        <h1 class="text-2xl font-bold">Welkom bij FTS!</h1>
+        <p>Bekijk hier alle festivals en plan je reis.</p>
+    </div>
+</x-app-layout>
